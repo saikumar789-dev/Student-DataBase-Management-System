458ecf840f162f7a923a5c5fcd8853bc750a5256
saikumar789-dev <120249257+saikumar789-dev@users.noreply.github.com>
2023-05-04 19:39:15 +0530

    Create student table.sql

---
+++ student table.sql
@@ -0,0 +1,8 @@
+create table(
+id     number(4) not null,
+name   varchar2(25) not null,
+mobile_no  number(10) not null,
+address    varchar2(50),
+branch     varchar2(10),
+doj        date,
+fees       number(8,2);
